d9fe2c49b5712bb5e12c0589c7fc7c980b052a3a
emm <723125@gmail.com>
2021-01-14 16:37:24 +0000

    Initial Commit

---
+++ facebookLogin_11/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  facebookLogin_11
+//
+//  Created by emm on 14/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ facebookLogin_11/facebookLogin_11App.swift
@@ -0,0 +1,17 @@
+//
+//  facebookLogin_11App.swift
+//  facebookLogin_11
+//
+//  Created by emm on 14/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct facebookLogin_11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
